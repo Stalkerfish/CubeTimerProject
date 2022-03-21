6180dcc635460ef122e00986fd85d75c17210f9a
Stalkerfish <silva.allison2002@gmail.com>
2022-03-21 03:05:22 -0300

    creates a file to storage Macros and common-knowledge info for the rest of the code

---
+++ Cube Timer/include/MACROS.h
@@ -0,0 +1,8 @@
+// This file contains references for the pins, peripherals, and whatever is needed.
+
+#pragma once
+
+#define LEFT_SENSOR 9
+#define RIGHT_SENSOR 10
+#define LED_READY 7
+#define LED_GO 8
